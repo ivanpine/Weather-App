8761a1cf96b2cd5939d4a8a97ee3e5bcc422b744
Ivan Pinevski <ivanpinevski87@gmail.com>
2024-06-05 23:19:36 +0200

    main

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const apiKey = "c0e52022c857a773ddd61e28d06baaa3";
 const searchInput = document.querySelector('.city-search__input');
 const searchButton = document.querySelector('.search__button');
 const form = document.querySelector('form');
@@ -8,23 +7,43 @@ const form = document.querySelector('form');
 const cityName = document.querySelector('.city');
 const cityTemperature = document.querySelector('.weather-temperature');
 const weatherDesc = document.querySelector('.weather-description');
+const humidity = document.querySelector('.row-1_value');
+const wind = document.querySelector('.row-2_value');
+const feelsLike = document.querySelector('.row-3_value');
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
 
 async function checkWeather(city) {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`);
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=6016ca3d7b8340d7aff135730240506&q=${city}&aqi=no`);
     const data = await response.json();
 
-    const lat = data[0].lat;
-    const lon = data[0].lon;
+    const lat = data.location.lat;
+    const lon = data.location.lon;
 
-    const response2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+    const response2 = await fetch(`https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}`);
     const data2 = await response2.json();
+
+    console.log(data);
 
     console.log(data2);
 
-    cityName.innerHTML = city;
-    cityTemperature.innerHTML = Math.round(data2.main.temp) + '°C';
-    weatherDesc.innerHTML = data2.weather[0].main;
+    console.log(typeof data2.results.sunrise);
 
+    cityName.innerHTML = data.location.name;
+    cityTemperature.innerHTML = Math.round(data.current.temp_c) + '°C';
+    weatherDesc.innerHTML = data.current.condition.text;
+    humidity.innerHTML = data.current.humidity + '%';
+    wind.innerHTML = Math.round(data.current.wind_kph) + 'km/h';
+    feelsLike.innerHTML = Math.round(data.current.feelslike_c) + '°C';
+
+    const sunriseTime = data2.results.sunrise;
+    const [hour, minute] = sunriseTime.split(':');
+    const formattedTime = hour.padStart(2, '0') + ':' + minute;
+    sunrise.innerHTML = formattedTime;
+    const sunsetTime = data2.results.sunset;
+    const [hour2, minute2] = sunsetTime.split(':');
+    const formattedTime2 = hour2.padStart(2, '0') + ':' + minute2;
+    sunset.innerHTML = formattedTime2;
 };
 
 form.addEventListener('submit', function(e) {
